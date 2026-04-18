511c8d1df06232bc4416a2924db604ab326c6e5f
shinebayar <shinebayarking.1996@gmail.com>
2026-04-18 22:14:57 +0800

    fix nested repo issue

---
+++ frontend/index.js
@@ -0,0 +1,21 @@
+const form=document.getElementById('loginform')
+
+form.addEventListener('submit', async (event)=>{
+
+    event.preventDefault()
+
+    const username=document.getElementById('username').value
+    const password=document.getElementById('password').value
+    const response= await fetch('/login',{
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({username,password})
+    })
+    
+    const data=await response.json()
+    document.getElementById('result').innerText = data.message;
+
+
+})
